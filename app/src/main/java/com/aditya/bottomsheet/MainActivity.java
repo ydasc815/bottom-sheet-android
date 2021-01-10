8721a6fbbb27487d3e3826f35169c90c3c308aed
Aditya <icgadi99@gmail.com>
2021-01-11 00:02:55 +0530

    --added: create base workflow

--- app/src/main/java/com/aditya/bottomsheet/MainActivity.java
+++ app/src/main/java/com/aditya/bottomsheet/MainActivity.java
@@ -1,14 +1,26 @@
 package com.aditya.bottomsheet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.aditya.bottomsheet.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BottomSheetBaseClass bottomSheet = new BottomSheetBaseClass();
+                bottomSheet.show(getSupportFragmentManager(), "tag");
+            }
+        });
     }
 }
